86dab6f9ba2ec9108ccf161eeb645d19562e1b5d
Code Sandbox <hello@codesandbox.io>
2023-01-08 18:14:09 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.16969c507f0e4d46f774524c520eaca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f14ef3d6ce0b7c2e8d6e4b614c8b02",
+    "url": "https://moomar232.github.io/csb-cou5sb/index.html"
+  },
+  {
+    "revision": "b03858aea640dfe3fd07",
+    "url": "https://moomar232.github.io/csb-cou5sb/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "3ad784398ec6227b399a",
+    "url": "https://moomar232.github.io/csb-cou5sb/static/js/2.93b78f3f.chunk.js"
+  },
+  {
+    "revision": "b03858aea640dfe3fd07",
+    "url": "https://moomar232.github.io/csb-cou5sb/static/js/main.393c535f.chunk.js"
+  },
+  {
+    "revision": "6092eea1fe58f272b5e1",
+    "url": "https://moomar232.github.io/csb-cou5sb/static/js/runtime~main.354725e0.js"
+  }
+]);
